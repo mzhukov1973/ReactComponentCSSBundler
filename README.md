3706859676c3b94fc8c4f582adf803b43fee20a7
UmbraFerreus <mzhukov31415dev@gmail.com>
2017-09-16 06:44:13 +0300

    Initial commit - first upload to GitHub, just a skeleton at this point. Also some very rudimentary description added to README.md, as well as licensing info.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ReactComponentCSSBundler
 A simple JS-based CSS bundling solution for React.js components to bind arbitrary CSS styling to a custom component without risking class namespace collisions.
+
+Can be easily added to component by hand or one may run a script to attempt to inject Bundler code into the component automatically.
+
+#### The full-auto script is provided to attempt to:
+- Scan a component,
+- Gather and consume its relevant CSS files,
+- Transform them for js-based storage,
+- Inject the resulting object into the components' code together with restoration routines,
+- Remove all references to said CSS files from the component,
+- And add a small blurb on usage somewhere inside.
+
+#### The semi-auto script is provided to attempt to:
+- Scan a component,
+- Gather and consume its relevant CSS files,
+- Transform them for js-based storage,
+- Output the resulting object either on stdout or to a file together with restoration routines, some installation instructions and hints,
+- To be added to the components' code manually.
+
---
+++ injectCSSBundler.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#===========================================================================#
+#  Copyright 2017 Maxim Zhukov                                              #
+#                                                                           #
+#  Licensed under the Apache License, Version 2.0 (the "License");          #
+#  you may not use this file except in compliance with the License.         #
+#  You may obtain a copy of the License at                                  #
+#                                                                           #
+#      http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                           #
+#  Unless required by applicable law or agreed to in writing, software      #
+#  distributed under the License is distributed on an "AS IS" BASIS,        #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+#  See the License for the specific language governing permissions and      #
+#  limitations under the License.                                           #
+#===========================================================================#
+
+#Attempts to inject CSS Bundler into a custom component.
+#(Full auto mode.)
+
+
+#------Options------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Most options are to be guessed, the only required option is the path to the components' file. 
+#However, since we have to rely at least partially on code analysis, one must provide full option fine-tuning capabilities to the user as well, in case we miss a trick or two.
+COMPONENT_FILENAME=''    #Filename of the component. Is assumed to reside in the same folder with this script.
+COMPONENT_PATH_REL='.'   #Path to components' file relative to this scripts' location. Defaults to ".".
+COMPONENT_CSS_FILES=''   #List of components' CSS files. If empty or not set, it is attempted to generate one from scanning the components' code.
+COMPONENT_CSS_CLASSES='' #List of components' CSS classes. If empty or not set, it is attempted to generate one from scanning the components' code and relevant CSS files.
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#------Functions----------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+#------Script itself------------------------------------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
